12e49764a56d5c88a406c51b24cd171d549af036
Mayank <mayankzalavadia@gmail.com>
2023-12-09 00:31:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+var postsData = null;
+fetch('https://devhooks.in/api/getarticles')
+    .then(data => data.json())
+    .then(pData => {
+        postsData = pData;        
+});
+document.getElementById('devsearch').addEventListener("keyup", (event) => {
+    const resultElement = document.getElementById('result');
+    resultElement.innerHTML = '';
+    const query = event.target.value.toLowerCase();
+    const squery = event.target.value.toLowerCase().split(" ");
+    if(query.length > 1){
+        let catpostsData = postsData.filter(
+            (post) =>
+            post.status == 1 &&
+            squery.every(
+                (q) =>
+                post.title.toLowerCase().split(" ").includes(q) ||
+                post.categories.toLowerCase().split(",").includes(q)
+            )
+        );
+        if (!Object.keys(catpostsData).length) {
+            catpostsData = postsData.filter(
+            (post) =>
+                post.status == 1 &&
+                squery.some(
+                (q) =>
+                    post.title.toLowerCase().split(" ").includes(q) ||
+                    post.categories.toLowerCase().split(",").includes(q)
+                )
+            );
+        }
+        if (!Object.keys(catpostsData).length) {
+            catpostsData = postsData.filter(
+            (post) =>
+                post.status == 1 &&
+                squery.some(
+                (q) =>
+                    post.title.toLowerCase().includes(q) ||
+                    post.categories.toLowerCase().split(",").includes(q)
+                )
+            );
+        }
+        let posts = [];
+        catpostsData.map((postData, index) => {
+            posts.push({ title: postData.title, id: postData.id });
+        });
+        posts.forEach(function (data, index) {
+            resultElement.innerHTML += '<li><a href="https://devhooks.in/blog/'+data.id+'" title="'+data.title+'" target="_blank">'+data.title+'</a></li>';
+        });
+    }
+});
